82947f0a2c4a90f67bac01310484602f80c03c9d
nicolaslazo <nlazo@est.frba.utn.edu.ar>
2019-11-18 23:06:00 -0300

    ;s agregados a Barco.js

--- Barco.js
+++ Barco.js
@@ -13,7 +13,7 @@ Barco.prototype.sosSaqueablePor = function(unPirata) {
 }
 
 Barco.prototype.esVulnerableA = function(otroBarco) {
-	return this.cantidadTripulantes() <= otroBarco.cantidadTripulantes() / 2
+	return this.cantidadTripulantes() <= otroBarco.cantidadTripulantes() / 2;
 }
 
 Barco.prototype.sosSaqueablePor = function(unPirata) {
@@ -21,7 +21,7 @@ Barco.prototype.sosSaqueablePor = function(unPirata) {
 }
 
 Barco.prototype.esVulnerableA = function(otroBarco) {
-	return this.cantidadTripulantes() <= otroBarco.cantidadTripulantes() / 2
+	return this.cantidadTripulantes() <= otroBarco.cantidadTripulantes() / 2;
 }
 Barco.prototype.cantidadTripulantes = function() {
 	return this.tripulantes.length;
@@ -32,74 +32,74 @@ Barco.prototype.todosPasadosDeGrog = function() {
 }
 
 Barco.prototype.puedeUnirse = function(unPirata) {
-	return this.hayLugar() && mision.esUtil(unPirata)
+	return this.hayLugar() && mision.esUtil(unPirata);
 }
 
 Barco.prototype.hayLugar = function() {
-	return this.cantidadTripulantes() < capacidad
+	return this.cantidadTripulantes() < capacidad;
 }
 
 Barco.prototype.agregar = function(unTripulante) {
 	if (this.puedeUnirse(unTripulante)) {
-		tripulantes.push(unTripulante)
+		tripulantes.push(unTripulante);
 	}
 }
 
 Barco.prototype.cambiarMision = function(unaMision) {
-	this.tripulantes = tripulantes.filter(tripulante => unaMision.esUtil(tripulante))
-	this.mision = unaMision
+	this.tripulantes = tripulantes.filter(tripulante => unaMision.esUtil(tripulante));
+	this.mision = unaMision;
 }
 
 Barco.prototype.pirataMasEbrio = function() {
-	return tripulantes.sort(tripulante => tripulante.nivelEbriedad).reverse()[0]
+	return tripulantes.sort(tripulante => tripulante.nivelEbriedad).reverse()[0];
 }
 
 Barco.prototype.anclarEn = function(unaCiudad) {
-	this.todosTomanGrog()
-	this.perderMasEbrioEn(unaCiudad)
+	this.todosTomanGrog();
+	this.perderMasEbrioEn(unaCiudad);
 }
 
 Barco.prototype.todosTomanGrog = function() {
-	this.tripulantes.forEach(tripulante => tripulante.tomarGrog())
+	this.tripulantes.forEach(tripulante => tripulante.tomarGrog());
 }
 
 Barco.prototype.perderMasEbrioEn = function(unaCiudad) {
-	this.tripulantes.pop(this.pirataMasEbrio)
-	unaCiudad.sumarHabitante()
+	this.tripulantes.pop(this.pirataMasEbrio);
+	unaCiudad.sumarHabitante();
 }
 
 Barco.prototype.esTemible = function() {
-	this.mision.esRealizablePor(this)
+	this.mision.esRealizablePor(this);
 }
 
 Barco.prototype.tieneSuficienteTripulacion = function() {
-	return this.cantidadTripulantes() >= capacidad * 0.9
+	return this.cantidadTripulantes() >= capacidad * 0.9;
 }
 
 Barco.prototype.tiene = function(unItem) {
-	return this.tripulantes.some(tripulante => tripulante.tiene(unItem))
+	return this.tripulantes.some(tripulante => tripulante.tiene(unItem));
 }
 
 Barco.prototype.cantidadTripulantesPasadosDeGrog = function() {
-	return this.tripulantesPasadosDeGrog().length
+	return this.tripulantesPasadosDeGrog().length;
 }
 
 Barco.prototype.cantidadItemsDistintosEntreTripulantesPasadosDeGrog = function() {
-	return this.tripulantesPasadosDeGrog().flatMap(tripulante => tripulante.items.asSet().size())
+	return this.tripulantesPasadosDeGrog().flatMap(tripulante => tripulante.items.asSet().size());
 }
 
 Barco.prototype.tripulantesPasadosDeGrog = function() {
-	return this.tripulantes.filter(tripulante => tripulante.pasadoDeGrog())
+	return this.tripulantes.filter(tripulante => tripulante.pasadoDeGrog());
 }
 
 Barco.prototype.tripulantePasadoDeGrogConMasMonedas = function() {
-	return tripulantesPasadosDeGrog().sort(tripulante => tripulante.cantidadMonedas()).reverse()[0]
+	return tripulantesPasadosDeGrog().sort(tripulante => tripulante.cantidadMonedas()).reverse()[0];
 }
 
 Barco.prototype.tripulanteMasInvitador = function() {
-	return this.tripulantes.sort(tripulante => tripulante.cantidadInvitadosPara(this)).reverse()[0]
+	return this.tripulantes.sort(tripulante => tripulante.cantidadInvitadosPara(this)).reverse()[0];
 }
 
 Barco.prototype.cantidadInvitadosPor = function(unTripulante) {
-	return this.tripulantes.filter(tripulante => tripulante.fuisteInvitadoPor(unTripulante)).length
+	return this.tripulantes.filter(tripulante => tripulante.fuisteInvitadoPor(unTripulante)).length;
 }
